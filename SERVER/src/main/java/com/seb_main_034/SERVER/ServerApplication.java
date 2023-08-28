0eb6872de1e19b6b4b91b84207b122a6f9e4b8e9
AlexinDPK <gim3187@gmail.com>
2023-08-28 09:10:39 +0900

    Add SERVER folder

---
+++ SERVER/src/main/java/com/seb_main_034/SERVER/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.seb_main_034.SERVER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
---
+++ SERVER/src/test/java/com/seb_main_034/SERVER/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seb_main_034.SERVER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

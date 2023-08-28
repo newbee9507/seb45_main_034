78cd99449b1f40f065dce74a9eb79c7a0245caa1
newbee9507 <cho6185@naver.com>
2023-08-28 15:58:45 +0900

    add PatchMapper and add Bean

--- SERVER/src/main/java/com/seb_main_034/SERVER/users/mapper/PatchMapper.java
+++ SERVER/src/main/java/com/seb_main_034/SERVER/users/mapper/PatchMapper.java
@@ -2,8 +2,12 @@ package com.seb_main_034.SERVER.users.mapper;
 
 import com.seb_main_034.SERVER.users.dto.UserPatchDto;
 import com.seb_main_034.SERVER.users.entity.Users;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface PatchMapper {
 
+    @Bean
     Users UserPatchDTOtoUser(Users user, UserPatchDto userPatchDto);
 }

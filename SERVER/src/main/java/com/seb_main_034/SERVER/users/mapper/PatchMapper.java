101a5ab896a49d325fbc87190f4362983b72dba8
newbee9507 <cho6185@naver.com>
2023-08-28 16:03:18 +0900

    add PatchMapper and not interface component

--- SERVER/src/main/java/com/seb_main_034/SERVER/users/mapper/PatchMapper.java
+++ SERVER/src/main/java/com/seb_main_034/SERVER/users/mapper/PatchMapper.java
@@ -5,9 +5,9 @@ import com.seb_main_034.SERVER.users.entity.Users;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public interface PatchMapper {
 
-    @Bean
+
     Users UserPatchDTOtoUser(Users user, UserPatchDto userPatchDto);
 }
--- SERVER/src/main/java/com/seb_main_034/SERVER/users/mapper/PatchMapperImp.java
+++ SERVER/src/main/java/com/seb_main_034/SERVER/users/mapper/PatchMapperImp.java
@@ -2,7 +2,9 @@ package com.seb_main_034.SERVER.users.mapper;
 
 import com.seb_main_034.SERVER.users.dto.UserPatchDto;
 import com.seb_main_034.SERVER.users.entity.Users;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PatchMapperImp implements PatchMapper{
 
     @Override
